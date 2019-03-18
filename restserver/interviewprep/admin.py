6e4008e69d3769f6b1bdea1e49b98922e6c08e94
vishnulearning <abhinav.dayal@vishnu.edu.in>
2019-03-18 11:41:14 +0530

    added models for interview prep

--- restserver/interviewprep/admin.py
+++ restserver/interviewprep/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import InterviewProfile
+from .models import InterviewProfile, Course, Lesson, Question, Attempt
 
 admin.site.register(InterviewProfile)
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Question)
+admin.site.register(Attempt)
--- restserver/interviewprep/models.py
+++ restserver/interviewprep/models.py
@@ -21,3 +21,51 @@ def create_user_interview_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_interview_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class Course(models.Model):
+   title = models.CharField(max_length=50)
+   description = models.CharField(max_length=1000)
+   users = models.ManyToManyField(User)
+   canpeek = models.BooleanField(default=True)
+   cost = models.FloatField(default=0)
+
+   def __str__(self):
+        return self.title
+
+   class Meta:
+        ordering = ('title',)
+
+
+class Lesson(models.Model):
+   title = models.CharField(max_length=50)
+   description = models.CharField(max_length=1000)
+   course = models.ForeignKey(Course, on_delete=models.CASCADE)
+   paid = models.BooleanField(default=False)
+
+   def __str__(self):
+        return self.title
+
+   class Meta:
+        ordering = ('title',)
+
+
+class Question(models.Model):
+   title = models.CharField(max_length=50)
+   question = models.CharField(max_length=500)
+   visemes = models.CharField(max_length=500)
+   answer = models.CharField(max_length=1000)
+   lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+   score = models.IntegerField(default=100)
+
+   def __str__(self):
+        return self.title
+
+   class Meta:
+        ordering = ('title',)
+
+class Attempt(models.Model):
+   question = models.ForeignKey(Question, on_delete=models.CASCADE)
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+   score = models.FloatField(default=0)
+   answer = models.CharField(max_length=2000)
